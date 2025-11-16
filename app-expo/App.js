9026ef5d24460d9d4b4d345206e0ac72da740dc6
Antonio Rubens O Junqueira <127354448+AntonioRubens77@users.noreply.github.com>
2025-11-16 19:31:37 -0300

    Add files via upload

---
+++ app-expo/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View,Text} from "react-native";
+import Homepage from "./src/pages/HomePage"
+
+const App = () => {
+  return (
+   <Homepage />
+
+  );
+
+}
+
+export default App;
